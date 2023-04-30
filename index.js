ba20cd4f4ee958698155d0964b2666e2c5989ed4
ozozod <ozozojuegos@gmail.com>
2023-04-30 01:19:37 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const API_KEY = 'f55567fd17dbcb71c85764a6ae1f5496';
+
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&exclude={part}&appid=${API_KEY}`)
+        .then( response => response.json())
+        .then(data => setWeatherData(data))
+}
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(), 
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+
+    cleanUp();
+}
+
+const  cleanUp = () => {
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex';
+
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${ ('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
